9bea50ff5a9c1337fcb2a964a6ed129c5a770515
fsato <satofaction@gmail.com>
2025-02-01 14:35:05 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/threejs-physics-sample01/", // リポジトリ名を指定
   build: {
     outDir: "docs", // ビルド出力ディレクトリを 'docs' に設定
   },
